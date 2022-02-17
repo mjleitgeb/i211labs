27eb7afdcf551a76a0a6cf9035a310a92c72450a
mleitgeb <mleitgeb@iu.edu>
2022-02-17 14:40:46 -0500

    lab3 3.3

---
+++ lab3/lab_03_starter/lab_03_starter.py
@@ -0,0 +1,21 @@
+from multiprocessing.spawn import old_main_modules
+import os
+
+
+# Part 1: write a short program that prints out the names of all of the contents 
+# of the examples/more_examples directory.
+
+print(os.listdir("/Users/maxleitgeb/Desktop/i211/I211_labs/lab3/lab_03_starter/examples/more_examples"))
+
+
+
+
+#Part 2: Change the current working directory to the resources/code/ directory.
+# Print out the names of all of the files (not directories!) and their sizes
+os.chdir(os.path.join(os.getcwd(), "resources/code/"))
+print(os.getcwd())
+print(os.listdir(os.getcwd()))
+
+
+#Part 3: Rename the functions.py file to database_functions.py.
+os.rename('functions.py', 'database_functions.py')
