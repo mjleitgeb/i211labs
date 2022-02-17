351629a7c20e5349a502a7f1755667a5e4ab5f77
mleitgeb <mleitgeb@iu.edu>
2022-02-17 13:46:10 -0500

    Updated Files

---
+++ lab03.py
@@ -0,0 +1,3 @@
+# Lab 03 - Max Leitgeb
+print('Lab 03: Git and Github')
+print('Linked a local repository to a remote repository')
--- lecture6.py
+++
@@ -1,2 +0,0 @@
-print('Lecture 6: Git and Github')
-print('Testing out the repo')
